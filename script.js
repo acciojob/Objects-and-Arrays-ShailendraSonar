acf6528bcc4104eaf1b346766b9c57b93f5450f6
Shailendra Sonar <77749654+ShailendraSonar@users.noreply.github.com>
2023-07-12 18:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,13 +6,9 @@ const person = {
 };
 
 
-const team = players;
-const  team1 = person;
-const cap1 = person;
-
-console.log(team);
-console.log(team1);
-console.log(cap1);
+var team = players;
+var team1 = [...players];
+var cap1 = {...person};
 
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
